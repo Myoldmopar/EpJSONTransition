f607f0939619021ceb7d71fc1221f96dba9a3d5f
Edwin Lee <leeed2001@gmail.com>
2021-07-30 22:40:12 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="EnergyPlus EpJSON Transition Tool",
-    version="0.1",
+    version="0.2",
     description="A tool for converting EpJSON files which may need changing from one version to another",
     url='https://energyplus.net',
     license='ModifiedBSD',
